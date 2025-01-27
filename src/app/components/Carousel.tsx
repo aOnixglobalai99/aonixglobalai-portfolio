69e952e0215ee2a59cf83db4f77a857c4061d717
AonixGlobalAI <support@anonixglobalai.com>
2025-01-28 00:12:37 +0530

    updated css

--- src/app/components/Carousel.tsx
+++ src/app/components/Carousel.tsx
@@ -9,88 +9,59 @@ const Carousel = () => {
   return (
     <div className="carousel">
       <div className="carouselItems">
-        <div className="carouselItem flex flex-col gap-[30px]">
-          <div className="pl-[10%] pr-[10%]">
-            "Working with AonixGlobalAI has significantly boosted our online
-            sales. Their robust e-commerce solutions have optimized our store,
-            providing a seamless shopping experience for our customers."
-          </div>
-          <Image
+        <div className="carouselItem">
+         "Working with AonixGlobalAI has significantly boosted our online sales. Their robust e-commerce solutions have optimized our store, providing a seamless shopping experience for our customers."
+         <Image
             src={client1}
             alt="Client 1"
-            className="h-[150px] w-[150px] rounded-[50%]"
           />
-          <div className="clientDetails">
-            <p className="clientName">John Doe</p>
-            <p className="clientTitle">Real Estate Consultant</p>
-            <p className="clientCompany">Prime Realty Co</p>
-          </div>
         </div>
-        <div className="carouselItem flex flex-col gap-[30px]">
-          <div className="pl-[10%] pr-[10%]">
-            "Thanks to AonixGlobalAI, our real estate business now runs smoothly
-            with a powerful platform that simplifies property listings, client
-            management, and transactions, driving our success in a competitive
-            market."
-          </div>
+        <div className="carouselItem">
+          "Thanks to AonixGlobalAI, our real estate business now runs smoothly with a powerful platform that simplifies property listings, client management, and transactions, driving our success in a competitive market."
           {/* <img
             src="https://www.shutterstock.com/image-photo/handsome-30s-businessman-employee-head-260nw-2156291093.jpg"
           /> */}
-          <Image
+           <Image
             src={client2}
             alt="Client 2"
-            // className="fixed top-[0] left-[0]"
-            className="h-[150px] w-[150px] rounded-[50%]"
           />
-          <div className="clientDetails">
+          {/* <div className="clientDetails">
             <p className="clientName">Jane Smith</p>
             <p className="clientTitle">Real Estate Consultant</p>
             <p className="clientCompany">Prime Realty Co</p>
-          </div>
+          </div> */}
         </div>
-        <div className="carouselItem flex flex-col gap-[30px]">
-          <div className="pl-[10%] pr-[10%]">
-            "The student management system built by AonixGlobalAI has
-            revolutionized how we track progress and manage administrative
-            tasks. It’s user-friendly, efficient, and has improved communication
-            across all departments."
-          </div>
-          <Image
+        <div className="carouselItem">
+         "The student management system built by AonixGlobalAI has revolutionized how we track progress and manage administrative tasks. It’s user-friendly, efficient, and has improved communication across all departments."
+          
+         <Image
             src={client3}
             alt="Client 3"
-            // className="fixed top-[0] left-[0]"
-            className="h-[150px] w-[150px] rounded-[50%]"
           />
           {/* <img
             src="https://www.shutterstock.com/image-photo/handsome-30s-businessman-employee-head-260nw-2156291093.jpg"
           /> */}
-          <div className="clientDetails">
+          {/* <div className="clientDetails">
             <p className="clientName">Alice Brown</p>
             <p className="clientTitle">Principal</p>
-            <p className="clientCompany">Future Scholars Academy</p>
-          </div>
+            <p className="clientCompany">Future Scholars Academy</p> */}
+          {/* </div> */}
         </div>
-        <div className="carouselItem flex flex-col gap-[30px]">
-          <div className="pl-[10%] pr-[10%]">
-            "With AonixGlobalAI’s travel booking solutions, our customers enjoy
-            a faster, more intuitive booking process. Their technology has
-            streamlined our operations and enhanced the travel experience for
-            our clients."
-          </div>
+        <div className="carouselItem">
+          "With AonixGlobalAI’s travel booking solutions, our customers enjoy a faster, more intuitive booking process. Their technology has streamlined our operations and enhanced the travel experience for our clients."
+         
           <Image
             src={client4}
             alt="Client 4"
-            // className="fixed top-[0] left-[0]"
-            className="h-[150px] w-[150px] rounded-[50%]"
           />
           {/* <img
             src="https://www.shutterstock.com/image-photo/handsome-30s-businessman-employee-head-260nw-2156291093.jpg"
           /> */}
-          <div className="clientDetails">
-            <p className="clientName">Michael Green</p>
+           {/* <div className="clientDetails"> */}
+            {/* <p className="clientName">Michael Green</p>
             <p className="clientTitle">Travel Agency Owner</p>
-            <p className="clientCompany">Wanderlust Travels</p>
-          </div>
+            <p className="clientCompany">Wanderlust Travels</p> */}
+          {/* </div> */}
         </div>
       </div>
     </div>
