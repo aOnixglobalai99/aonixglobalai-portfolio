85398043178a72cc4f9b6204643cf10b0c692331
gptshubham <shubhamca76@gmail.com>
2025-01-28 17:02:07 +0000

    deployment error fixed

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 // import { FaChevronDown } from 'react-icons/fa';
