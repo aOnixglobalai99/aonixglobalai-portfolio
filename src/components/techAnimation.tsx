adf664237104b5c36ed703bf563da82e823e9b20
gptshubham <shubhamca76@gmail.com>
2025-02-07 19:25:46 +0000

    deployment error fix

--- src/components/techAnimation.tsx
+++ src/components/techAnimation.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import { div } from 'framer-motion/client';
+// import { div } from 'framer-motion/client';
 import Image from 'next/image';
 
 const techStack1 = [
