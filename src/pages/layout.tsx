40f7fc49bec4e7f0747b840983374940c8eee6f6
gptshubham <shubhamca76@gmail.com>
2025-02-07 18:47:05 +0000

    deployment error fix

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from 'next/font/google';
 // import './globals.css';
-import { metadata } from './layout.server';
+import { metadata } from '../layout.server';
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
