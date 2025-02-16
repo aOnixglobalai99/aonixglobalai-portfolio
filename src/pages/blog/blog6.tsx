39a7bbfef007bf9ef516019720b22f7865b1cbf1
gptshubham <shubhamca76@gmail.com>
2025-02-16 14:00:41 +0000

    blog 6 responsiveness added and style updated

--- src/pages/blog/blog6.tsx
+++ src/pages/blog/blog6.tsx
@@ -4,39 +4,46 @@ import articles from '@/components/blog/BlogList';
 
 const AISportsFitnessPage: React.FC = () => {
   return (
-    <div className="flex gap-10 min-h-screen p-8">
-      <div className="h-fit mx-auto p-8 rounded-lg shadow-lg bg-[#F7FFFF]">
+    <div className="flex flex-col-reverse lg:flex-row gap-4 min-h-screen p-1 lg:p-8">
+      {/* Sidebar with Recent Blogs */}
+      <div className="w-full lg:w-[460px] h-fit mx-auto p-4 lg:p-8 rounded-lg bg-[#F7FFFF] mb-[100px] border shadow-sm sticky top-[100px]">
         <h1 className="text-2xl mb-5">Recent Blogs</h1>
         {articles.slice(0, 3).map((article) => (
-          <div className="flex gap-5 mb-5" key={article.blogPath}>
-            <img src={article.imageUrl} alt="" className="h-20 w-32" />
-            <div className="w-[60%]">
-              <Link
-                href={article.blogPath}
-                className=" hover:text-blue-600 transition"
-              >
+          <div
+            className="flex lg:flex-col 2xl:flex-row gap-3 lg:gap-5 mb-5"
+            key={article.blogPath}
+          >
+            <img
+              src={article.imageUrl}
+              alt=""
+              className="h-16 lg:h-20 w-24 lg:w-32 object-cover"
+            />
+            <div className="flex-1 lg:w-[100%] hover:text-cyan-500 transition">
+              <Link href={article.blogPath} className="text-sm lg:text-base">
                 {article.title}
               </Link>
             </div>
           </div>
         ))}
       </div>
-      <div className="max-w-4xl mx-auto bg-[#F7FFFF] p-8 rounded-lg shadow-lg">
+
+      {/* Main Blog Content */}
+      <div className="w-full lg:max-w-4xl mx-auto p-4 lg:p-8 rounded-lg bg-[#F7FFFF] mb-[20px] lg:mb-[100px] border shadow-sm">
         <div>
           <img
             src="/images/blogImages/ai_sports_fitness_blog_1.avif"
             alt=""
-            className="w-full h-[400px]"
+            className="w-full h-48 md:h-[300px] lg:h-[400px] object-cover"
           />
         </div>
-        <h1 className="text-4xl font-bold text-center mb-8 text-gray-800 mt-[50px]">
+        <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-center mb-6 lg:mb-8 text-gray-800 mt-6 lg:mt-[50px]">
           AI in Sports & Fitness: Transforming Performance, Training, and Health
         </h1>
-        <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+        <h2 className="text-xl lg:text-2xl font-semibold text-gray-800 mb-4">
           Introduction
         </h2>
 
-        <p className="text-gray-700 mb-6">
+        <p className="text-sm lg:text-base text-gray-700 mb-6">
           Artificial Intelligence (AI) is revolutionizing the sports and fitness
           industry, enhancing athlete performance, optimizing training, and
           improving fan engagement. With AI-powered analytics, wearable
@@ -45,10 +52,10 @@ const AISportsFitnessPage: React.FC = () => {
           never before.
         </p>
 
-        <h2 className="text-2xl font-semibold mt-8 mb-4 text-gray-800">
+        <h2 className="text-xl lg:text-2xl font-semibold text-gray-800 mb-4">
           The Role of AI in Sports & Fitness
         </h2>
-        <p className="text-gray-700 mb-6">
+        <p className="text-sm lg:text-base text-gray-700 mb-6">
           AI is transforming the way athletes train, teams strategize, and fans
           experience sports. From tracking player movements to analyzing
           biomechanical data, AI enhances efficiency, minimizes injuries, and
@@ -57,22 +64,22 @@ const AISportsFitnessPage: React.FC = () => {
           players, teams, coaches, and fans alike.
         </p>
 
-        <h2 className="text-2xl font-semibold mt-8 mb-4 text-gray-800">
+        <h2 className="text-xl lg:text-2xl font-semibold text-gray-800 mb-4">
           Key Applications of AI in Sports & Fitness
         </h2>
 
         <div className="space-y-6">
           <div>
-            <h3 className="text-xl font-semibold mb-2 text-gray-900">
+            <h3 className="text-lg lg:text-xl font-bold text-gray-900 mb-2">
               AI-Powered Performance Analytics
             </h3>
-            <p className="text-gray-700">
+            <p className="text-sm lg:text-base text-gray-700">
               Professional sports teams and fitness professionals use AI-driven
               analytics to measure performance and optimize training. AI
               analyzes vast amounts of data from matches, training sessions, and
               physiological metrics to provide actionable insights.
             </p>
-            <ul className="list-disc list-inside text-gray-700 mt-2">
+            <ul className="list-disc list-inside text-sm lg:text-base text-gray-700 mt-2">
               <li>
                 <strong>Motion Tracking & Biomechanics Analysis:</strong>{' '}
                 AI-powered cameras and sensors track athletes&apos; movements in
@@ -98,15 +105,15 @@ const AISportsFitnessPage: React.FC = () => {
           </div>
 
           <div>
-            <h3 className="text-xl font-semibold mb-2 text-gray-900">
+            <h3 className="text-lg lg:text-xl font-bold text-gray-900 mb-2">
               Wearable AI & Smart Fitness Devices
             </h3>
-            <p className="text-gray-700">
+            <p className="text-sm lg:text-base text-gray-700">
               AI-integrated wearables have transformed fitness tracking and
               health monitoring. These devices collect and analyze real-time
               biometric data, providing personalized insights.
             </p>
-            <ul className="list-disc list-inside text-gray-700 mt-2">
+            <ul className="list-disc list-inside text-sm lg:text-base text-gray-700 mt-2">
               <li>
                 <strong>Heart Rate & Oxygen Monitoring:</strong> Devices like
                 Apple Watch, Fitbit, and WHOOP use AI to track vital health
@@ -130,14 +137,14 @@ const AISportsFitnessPage: React.FC = () => {
           </div>
 
           <div>
-            <h3 className="text-xl font-semibold mb-2 text-gray-900">
+            <h3 className="text-lg lg:text-xl font-bold text-gray-900 mb-2">
               Injury Prevention & Rehabilitation
             </h3>
-            <p className="text-gray-700">
+            <p className="text-sm lg:text-base text-gray-700">
               AI helps prevent sports injuries by analyzing movement patterns
               and detecting potential risks.
             </p>
-            <ul className="list-disc list-inside text-gray-700 mt-2">
+            <ul className="list-disc list-inside text-sm lg:text-base text-gray-700 mt-2">
               <li>
                 <strong>AI-Driven Motion Capture:</strong> Systems like Kinexon
                 and Catapult assess muscle strain, joint movements, and fatigue
@@ -162,21 +169,21 @@ const AISportsFitnessPage: React.FC = () => {
           </div>
 
           <div>
-            <h3 className="text-xl font-semibold mb-2 text-gray-900">
+            <h3 className="text-lg lg:text-xl font-bold text-gray-900 mb-2">
               AI-Enhanced Coaching & Training
             </h3>
             <div>
               <img
                 src="/images/blogImages/ai_sports_fitness_blog_2.avif"
                 alt=""
-                className="w-full h-[400px] pb-[50px]"
+                className="w-full h-48 md:h-[300px] lg:h-[400px] object-cover mb-6 lg:mb-12"
               />
             </div>
-            <p className="text-gray-700">
+            <p className="text-sm lg:text-base text-gray-700">
               Coaches and trainers leverage AI-driven platforms to design
               personalized training programs.
             </p>
-            <ul className="list-disc list-inside text-gray-700 mt-2">
+            <ul className="list-disc list-inside text-sm lg:text-base text-gray-700 mt-2">
               <li>
                 <strong>AI-Based Virtual Coaches:</strong> AI apps like
                 Freeletics and Tempo provide personalized coaching based on user
@@ -201,14 +208,14 @@ const AISportsFitnessPage: React.FC = () => {
           </div>
 
           <div>
-            <h3 className="text-xl font-semibold mb-2 text-gray-900">
+            <h3 className="text-lg lg:text-xl font-bold text-gray-900 mb-2">
               AI in Fan Engagement & Sports Broadcasting
             </h3>
-            <p className="text-gray-700">
+            <p className="text-sm lg:text-base text-gray-700">
               AI-driven solutions enhance fan experience through immersive
               technologies and personalized content.
             </p>
-            <ul className="list-disc list-inside text-gray-700 mt-2">
+            <ul className="list-disc list-inside text-sm lg:text-base text-gray-700 mt-2">
               <li>
                 <strong>AI-Powered Highlights & Analysis:</strong> AI-generated
                 match summaries highlight key moments for fans.
@@ -231,14 +238,14 @@ const AISportsFitnessPage: React.FC = () => {
           </div>
 
           <div>
-            <h3 className="text-xl font-semibold mb-2 text-gray-900">
+            <h3 className="text-lg lg:text-xl font-bold text-gray-900 mb-2">
               AI in E-Sports & Gaming
             </h3>
-            <p className="text-gray-700">
+            <p className="text-sm lg:text-base text-gray-700">
               AI is also transforming e-sports by analyzing gaming strategies,
               improving in-game AI opponents, and optimizing player performance.
             </p>
-            <ul className="list-disc list-inside text-gray-700 mt-2">
+            <ul className="list-disc list-inside text-sm lg:text-base text-gray-700 mt-2">
               <li>
                 <strong>AI Opponents & Simulations:</strong> AI-powered
                 opponents adapt to players&apos; strategies for a more realistic
@@ -261,14 +268,14 @@ const AISportsFitnessPage: React.FC = () => {
           </div>
 
           <div>
-            <h3 className="text-xl font-semibold mb-2 text-gray-900">
+            <h3 className="text-lg lg:text-xl font-bold text-gray-900 mb-2">
               AI for Personalized Nutrition & Diet Plans
             </h3>
-            <p className="text-gray-700">
+            <p className="text-sm lg:text-base text-gray-700">
               AI is also playing a role in optimizing diet plans for athletes
               and fitness enthusiasts.
             </p>
-            <ul className="list-disc list-inside text-gray-700 mt-2">
+            <ul className="list-disc list-inside text-sm lg:text-base text-gray-700 mt-2">
               <li>
                 <strong>AI-Driven Nutrition Planning:</strong> AI assesses
                 individual dietary needs and recommends meal plans based on
@@ -288,15 +295,15 @@ const AISportsFitnessPage: React.FC = () => {
           </div>
         </div>
 
-        <h2 className="text-2xl font-semibold mt-8 mb-4 text-gray-800">
+        <h2 className="text-xl lg:text-2xl font-semibold text-gray-800 my-4">
           How IT Companies Can Leverage AI in Sports & Fitness
         </h2>
-        <p className="text-gray-700 mb-6">
+        <p className="text-sm lg:text-base text-gray-700 mb-6">
           AI presents immense opportunities for IT companies to develop
           innovative solutions in sports technology. Here&apos;s how your
           company can contribute:
         </p>
-        <ul className="list-disc list-inside text-gray-700 mt-2">
+        <ul className="list-disc list-inside text-sm lg:text-base text-gray-700 mt-2">
           <li>
             <strong>Develop AI-Powered Training Platforms:</strong> Create
             intelligent coaching systems that analyze motion and suggest
@@ -332,10 +339,10 @@ const AISportsFitnessPage: React.FC = () => {
           </li>
         </ul>
 
-        <h2 className="text-2xl font-semibold mt-8 mb-4 text-gray-800">
+        <h2 className="text-xl lg:text-2xl font-semibold text-gray-800 my-4">
           Conclusion
         </h2>
-        <p className="text-gray-700 mb-6">
+        <p className="text-sm lg:text-base text-gray-700 mb-6">
           AI is revolutionizing sports and fitness, enabling athletes to reach
           peak performance, reducing injury risks, and enhancing fan engagement.
           As an IT company, investing in AI-driven sports technology opens doors
@@ -343,7 +350,7 @@ const AISportsFitnessPage: React.FC = () => {
           solutions, you can contribute to the next wave of advancements in the
           sports and fitness industry.
         </p>
-        <p className="text-gray-700 mb-6">
+        <p className="text-sm lg:text-base text-gray-700">
           Are you ready to integrate AI into your sports and fitness solutions?
           Let&apos;s build the future of AI-driven athletics together!
         </p>
