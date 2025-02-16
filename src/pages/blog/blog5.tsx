110400097c6c2fa925c7c57bf0c15a14a5a03d06
gptshubham <shubhamca76@gmail.com>
2025-02-16 12:58:55 +0000

    blog 5 responsiveness added and style update

--- src/pages/blog/blog5.tsx
+++ src/pages/blog/blog5.tsx
@@ -4,39 +4,46 @@ import articles from '@/components/blog/BlogList';
 
 const AI_Powered_Agriculture = () => {
   return (
-    <div className="flex gap-10 min-h-screen p-8">
-      <div className="h-fit mx-auto p-8 rounded-lg shadow-lg bg-[#F7FFFF]">
+    <div className="flex flex-col-reverse lg:flex-row gap-4 min-h-screen p-1 lg:p-8">
+      {/* Sidebar with Recent Blogs */}
+      <div className="w-full lg:w-[460px] h-fit mx-auto p-4 lg:p-8 rounded-lg bg-[#F7FFFF] mb-[100px] border shadow-sm sticky top-[100px]">
         <h1 className="text-2xl mb-5">Recent Blogs</h1>
         {articles.slice(0, 3).map((article) => (
-          <div className="flex gap-5 mb-5" key={article.blogPath}>
-            <img src={article.imageUrl} alt="" className="h-20 w-32" />
-            <div className="w-[60%]">
-              <Link
-                href={article.blogPath}
-                className=" hover:text-blue-600 transition"
-              >
+          <div
+            className="flex lg:flex-col 2xl:flex-row gap-3 lg:gap-5 mb-5"
+            key={article.blogPath}
+          >
+            <img
+              src={article.imageUrl}
+              alt=""
+              className="h-16 lg:h-20 w-24 lg:w-32 object-cover"
+            />
+            <div className="flex-1 lg:w-[100%] hover:text-cyan-500 transition">
+              <Link href={article.blogPath} className="text-sm lg:text-base">
                 {article.title}
               </Link>
             </div>
           </div>
         ))}
       </div>
-      <div className="max-w-4xl mx-auto bg-[#F7FFFF] p-8 rounded-lg shadow-lg">
+
+      {/* Main Blog Content */}
+      <div className="w-full lg:max-w-4xl mx-auto p-4 lg:p-8 rounded-lg bg-[#F7FFFF] mb-[20px] lg:mb-[100px] border shadow-sm">
         <div>
           <img
             src="/images/blogImages/ai_agri_blog_1.avif"
             alt=""
-            className="w-full h-[400px]"
+            className="w-full h-48 md:h-[300px] lg:h-[400px] object-cover"
           />
         </div>
-        <h1 className="text-4xl font-bold text-center mb-8 text-gray-800 mt-[50px]">
+        <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-center mb-6 lg:mb-8 text-gray-800 mt-6 lg:mt-[50px]">
           AI-Powered Agriculture: Transforming Farming with Smart Technology
         </h1>
-        <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+        <h2 className="text-xl lg:text-2xl font-semibold text-gray-800 mb-4">
           Introduction
         </h2>
 
-        <p className="text-gray-700 mb-6">
+        <p className="text-sm lg:text-base text-gray-700 mb-6">
           Artificial Intelligence (AI) is revolutionizing the agriculture
           industry by optimizing crop management, improving yield predictions,
           and automating farming operations. AI-powered technologies such as
@@ -47,15 +54,15 @@ const AI_Powered_Agriculture = () => {
 
         {/* AI-Powered Crop Monitoring & Disease Detection */}
         <section className="mb-8">
-          <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+          <h2 className="text-xl lg:text-2xl font-semibold text-gray-800 mb-4">
             AI-Powered Crop Monitoring & Disease Detection
           </h2>
-          <p className="text-gray-700 mb-4">
+          <p className="text-sm lg:text-base text-gray-700 mb-4">
             AI-driven drones and sensors analyze soil health, monitor crop
             growth, and detect anomalies in real time. These technologies help
             farmers identify issues early and take corrective actions.
           </p>
-          <ul className="list-disc list-inside text-gray-700">
+          <ul className="list-disc list-inside text-sm lg:text-base text-gray-700">
             <li>
               <strong>Smart Crop Monitoring:</strong> AI-based aerial imagery
               analysis to detect pest infestations and nutrient deficiencies in
@@ -74,15 +81,15 @@ const AI_Powered_Agriculture = () => {
 
         {/* AI-Driven Precision Agriculture */}
         <section className="mb-8">
-          <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+          <h2 className="text-xl lg:text-2xl font-semibold text-gray-800 mb-4">
             AI-Driven Precision Agriculture
           </h2>
-          <p className="text-gray-700 mb-4">
+          <p className="text-sm lg:text-base text-gray-700 mb-4">
             Precision agriculture uses AI to optimize farming practices,
             ensuring efficient use of resources like water, fertilizers, and
             pesticides.
           </p>
-          <ul className="list-disc list-inside text-gray-700">
+          <ul className="list-disc list-inside text-sm lg:text-base text-gray-700">
             <li>
               <strong>Automated Irrigation & Water Management:</strong> AI
               optimizes water usage by analyzing soil moisture levels and
@@ -100,15 +107,15 @@ const AI_Powered_Agriculture = () => {
 
         {/* AI-Powered Yield Prediction & Crop Planning */}
         <section className="mb-8">
-          <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+          <h2 className="text-xl lg:text-2xl font-semibold text-gray-800 mb-4">
             AI-Powered Yield Prediction & Crop Planning
           </h2>
-          <p className="text-gray-700 mb-4">
+          <p className="text-sm lg:text-base text-gray-700 mb-4">
             AI helps farmers predict crop yields and plan their farming
             activities more effectively, reducing risks and maximizing
             productivity.
           </p>
-          <ul className="list-disc list-inside text-gray-700">
+          <ul className="list-disc list-inside text-sm lg:text-base text-gray-700">
             <li>
               <strong>Predictive Yield Analysis:</strong> Machine learning
               models forecast crop yields based on historical data, weather
@@ -128,22 +135,22 @@ const AI_Powered_Agriculture = () => {
 
         {/* AI-Powered Autonomous Farming Machinery */}
         <section className="mb-8">
-          <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+          <h2 className="text-xl lg:text-2xl font-semibold text-gray-800 mb-4">
             AI-Powered Autonomous Farming Machinery
           </h2>
           <div>
             <img
               src="/images/blogImages/ai_agri_blog_2.avif"
               alt=""
-              className="w-full h-[400px] pb-[50px]"
+              className="w-full h-48 md:h-[300px] lg:h-[400px] object-cover mb-6 lg:mb-12"
             />
           </div>
-          <p className="text-gray-700 mb-4">
+          <p className="text-sm lg:text-base text-gray-700 mb-4">
             Autonomous farming machinery powered by AI is transforming
             large-scale farming operations, reducing labor costs, and increasing
             efficiency.
           </p>
-          <ul className="list-disc list-inside text-gray-700">
+          <ul className="list-disc list-inside text-sm lg:text-base text-gray-700">
             <li>
               <strong>AI-Driven Tractors & Harvesters:</strong> Autonomous
               tractors and harvesters use computer vision and GPS for automated
@@ -163,14 +170,14 @@ const AI_Powered_Agriculture = () => {
 
         {/* AI in Supply Chain & Market Analytics */}
         <section className="mb-8">
-          <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+          <h2 className="text-xl lg:text-2xl font-semibold text-gray-800 mb-4">
             AI in Supply Chain & Market Analytics
           </h2>
-          <p className="text-gray-700 mb-4">
+          <p className="text-sm lg:text-base text-gray-700 mb-4">
             AI is optimizing the agricultural supply chain, reducing food waste,
             and helping farmers get the best prices for their produce.
           </p>
-          <ul className="list-disc list-inside text-gray-700">
+          <ul className="list-disc list-inside text-sm lg:text-base text-gray-700">
             <li>
               <strong>Demand & Price Forecasting:</strong> AI predicts market
               trends, helping farmers get the best prices for their produce.{' '}
@@ -189,14 +196,14 @@ const AI_Powered_Agriculture = () => {
 
         {/* AI-Powered Smart Greenhouses */}
         <section className="mb-8">
-          <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+          <h2 className="text-xl lg:text-2xl font-semibold text-gray-800 mb-4">
             AI-Powered Smart Greenhouses
           </h2>
-          <p className="text-gray-700 mb-4">
+          <p className="text-sm lg:text-base text-gray-700 mb-4">
             Smart greenhouses use AI to create optimal growing conditions for
             plants, increasing crop yields and reducing resource usage.
           </p>
-          <ul className="list-disc list-inside text-gray-700">
+          <ul className="list-disc list-inside text-sm lg:text-base text-gray-700">
             <li>
               <strong>Automated Climate Control:</strong> AI adjusts
               temperature, humidity, and CO2 levels to create optimal
@@ -215,14 +222,14 @@ const AI_Powered_Agriculture = () => {
 
         {/* AI-Based Farm Management Systems */}
         <section className="mb-8">
-          <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+          <h2 className="text-xl lg:text-2xl font-semibold text-gray-800 mb-4">
             AI-Based Farm Management Systems
           </h2>
-          <p className="text-gray-700 mb-4">
+          <p className="text-sm lg:text-base text-gray-700 mb-4">
             AI-powered farm management systems provide actionable insights for
             better decision-making, helping farmers optimize their operations.
           </p>
-          <ul className="list-disc list-inside text-gray-700">
+          <ul className="list-disc list-inside text-sm lg:text-base text-gray-700">
             <li>
               <strong>AI-Powered Decision Support Systems:</strong> AI analyzes
               farm data to provide actionable insights for better
@@ -241,14 +248,14 @@ const AI_Powered_Agriculture = () => {
 
         {/* AI for Sustainable Agriculture */}
         <section className="mb-8">
-          <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+          <h2 className="text-xl lg:text-2xl font-semibold text-gray-800 mb-4">
             AI for Sustainable Agriculture
           </h2>
-          <p className="text-gray-700 mb-4">
+          <p className="text-sm lg:text-base text-gray-700 mb-4">
             AI is helping farmers adopt sustainable practices, reduce waste, and
             adapt to climate change.
           </p>
-          <ul className="list-disc list-inside text-gray-700">
+          <ul className="list-disc list-inside text-sm lg:text-base text-gray-700">
             <li>
               <strong>Climate-Resilient Farming:</strong> AI helps farmers adapt
               to climate change by predicting weather patterns and suggesting
@@ -268,10 +275,10 @@ const AI_Powered_Agriculture = () => {
 
         {/* Conclusion */}
         <section>
-          <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+          <h2 className="text-xl lg:text-2xl font-semibold text-gray-800 mb-4">
             Conclusion
           </h2>
-          <p className="text-gray-700 mb-4">
+          <p className="text-sm lg:text-base text-gray-700 mb-4">
             AI is revolutionizing agriculture by increasing efficiency, reducing
             environmental impact, and improving food security. From autonomous
             farming machines to AI-driven pest control, the integration of AI
@@ -279,7 +286,7 @@ const AI_Powered_Agriculture = () => {
             practices. As AI continues to evolve, its role in agriculture will
             only become more critical in addressing global food challenges.
           </p>
-          <p className="text-gray-700">
+          <p className="text-sm lg:text-base text-gray-700">
             Are you ready to embrace AI in farming? The future of agriculture is
             intelligent and data-driven!
           </p>
